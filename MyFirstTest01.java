56552f1714c88c0caa6dc2302a491838ec309ea5
ChenHuiFannie <ruanchenhjy@163.com>
2022-10-24 16:47:58 +0800

    创建 MyFirstTest01 "hello world"

---
+++ MyFirstTest01.java
@@ -0,0 +1,10 @@
+/**
+ * @author: Mr.Strong
+ * @description: TODO
+ * @date: 2022/10/24 16:44
+ */
+public class MyFirstTest01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
